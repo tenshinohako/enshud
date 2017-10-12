300ec9d685024cc0bff77eb76949f62ec0fc40c7
ryohcmc <ryohkoyama42495@gmail.com>
2017-10-12 15:19:42 +0900

    from cmc

--- src/main/java/enshud/s2/parser/ProgramModel.java
+++ src/main/java/enshud/s2/parser/ProgramModel.java
@@ -5,31 +5,33 @@ import java.util.ArrayList;
 public class ProgramModel {
 	private ArrayList<Integer> tokenList;
 	private Integer pointer = new Integer(0);
-	
+
 	public ProgramModel(ArrayList<Integer> list) {
 		tokenList = list;
 	}
-	
-	
+
+
 	public void programRoot() {
 		header();
 		block();
 		compoundStatement();
 	}
-	
+
 	private void header() {
 		switch(tokenList.get(pointer)) {
 		case 17:
 			break;
 		}
 	}
-	
+
 	private void block() {
-		
+
 	}
-	
+
 	private void compoundStatement() {
-		
+
+
+
 	}
 
 }
