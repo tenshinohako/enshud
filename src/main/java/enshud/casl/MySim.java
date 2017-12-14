ab967daab9137bcaa54d22a3eaf9c6442db25dda
09B15028 <09B15028@localhost>
2017-12-14 14:31:52 +0900

    make CASL2

---
+++ src/main/java/enshud/casl/MySim.java
@@ -0,0 +1,12 @@
+package enshud.casl;
+
+import java.io.IOException;
+
+public class MySim extends CaslSimulator{	
+	public static void main(final String[] args) throws IOException {
+		//CaslSimulator.run("data/cas/normal01.cas", "tmp/out.ans");
+		//CaslSimulator.run("data/cas/normal04.cas", "tmp/out.ans", "8", "12");
+		CaslSimulator.run("data/cas/mycas.cas", "tmp/out.ans");
+	}
+
+}
