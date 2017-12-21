ce5ec5988e78b8500cbe558f33f5f48d0613a839
09B15028 <09B15028@localhost>
2017-12-21 14:29:54 +0900

    cas

--- src/main/java/enshud/casl/MySim.java
+++ src/main/java/enshud/casl/MySim.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 
 public class MySim extends CaslSimulator{	
 	public static void main(final String[] args) throws IOException {
-		//CaslSimulator.run("data/cas/normal01.cas", "tmp/out.ans");
+		//CaslSimulator.run("data/cas/normal01.cas", "tmp/out1.ans");
 		//CaslSimulator.run("data/cas/normal04.cas", "tmp/out.ans", "8", "12");
 		CaslSimulator.run("data/cas/mycas.cas", "tmp/out.ans");
 	}
